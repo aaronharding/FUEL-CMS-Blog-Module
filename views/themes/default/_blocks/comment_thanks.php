e3af8f1f5c2b83232d3168909a154b61b0554a3e
Aaron <hiitsaaron@googlemail.com>
2015-10-10 18:11:06 +0200

    changing comment title element to h6

--- views/themes/default/_blocks/comment_thanks.php
+++ views/themes/default/_blocks/comment_thanks.php
@@ -1,4 +1,4 @@
-<h3><?=lang('blog_comment_thanks')?></h3>
+<h6><?=lang('blog_comment_thanks')?></h6>
 <?php if ($this->fuel->blog->config('monitor_comments')){ ?>
 	<p class="success"><?=lang('blog_comments_monitored')?></p>
 <?php } ?>
